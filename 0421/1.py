9d41a70be3dc300fad740a89fb5d96eb7a3e388d
David95406 <83376469+David95406@users.noreply.github.com>
2022-04-21 13:51:27 +0200

    python

---
+++ 0421/1.py
@@ -0,0 +1,32 @@
+osszeredmeny = []
+f1 = open("0421/meccs.txt", "r", encoding="utf8()")
+f1.readline()
+class focimeccs:
+    fordulo: int
+    hfordulo: int
+    vfordulo: int
+    hfelidogol: int    
+    vfelidogol: int
+    hcsapat: str
+    vcsapat: str
+    
+    def __init__(self, sor: str) -> None:
+        adatok = sor.strip().split(" ")
+        self.fordulo = int(adatok[1])
+        self.hfordulo = int(adatok[1])
+        self.vfordulo = int(adatok[2])
+        self.hfelidogol = int(adatok[3])
+        self.hfelidogol = int(adatok[4])
+        self.hcsapat = adatok[5]
+        self.vcsapat = adatok[6]
+        
+for sor in f1:
+    egyeredmeny = focimeccs(sor.strip())
+    osszeredmeny.append(egyeredmeny)
+    
+print(f"A lista hossza: {len(osszeredmeny)}")
+fszam = int(input("Addja meg a fordulok számát 1 és 20 között."))
+
+for egyEredmeny in osszeredmeny:
+    if egyEredmeny.fordulo == fszam:
+        print(f"{egyEredmeny.hcsapat} - {egyEredmeny.vcsapat}")
