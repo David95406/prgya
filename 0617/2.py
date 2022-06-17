06d5f4daf635b5a4400b3b6f01854b79440e755e
David95406 <83376469+David95406@users.noreply.github.com>
2022-06-17 10:53:48 +0200

    0617
    agazati gyak

--- 0617/2.py
+++ 0617/2.py
@@ -3,18 +3,23 @@ from xmlrpc.client import boolean
 
 fszam = 1
 szamok = []
+paratlanok = []
+
+def minparatlan(lista):
+    return min(lista)
 
 while fszam > -1:
     fszam = int(input("$"))
+    if fszam % 2 != 0:
+        paratlanok.append(fszam)
     szamok.append(fszam)
 szamok.remove(fszam)
+paratlanok.remove(fszam)
 
-print(len(szamok))
+print("\n\n")
+print(f"A lista hossza: {len(szamok)}")
 
-def minparatlan(szam) -> boolean:
-    return szam % 2 != 0
+paratlanok.sort()
 
-szamok.sort()
-
-for a in range(len(szamok)):
-    print()
+print(paratlanok[-1])
+print(minparatlan(paratlanok))
