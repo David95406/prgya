111d8fb4cb54863f6dbc09ed8c0781b02fb6dba0
David95406 <83376469+David95406@users.noreply.github.com>
2023-09-05 13:12:52 +0200

    SZEPT. 5. Ismétlő feladat

---
+++ 12/WEB/0905 Ismétlő feladat/regi/script.js
@@ -0,0 +1,66 @@
+const szamlak = []
+var minOsszegValue
+var maxOsszegValue
+
+function addBill() {    
+    const szamla = {
+        cimzett,
+        datum,
+        osszeg
+    };
+
+    let cimzettInput = document.getElementById("cimzett").value
+    let datumInput = document.getElementById("datum").value
+    let osszegInput = document.getElementById("osszeg").value
+
+    szamla.cimzett = cimzettInput 
+    szamla.datum = datumInput 
+    szamla.osszeg = osszegInput
+
+    szamlak.push(szamla)
+
+
+
+    var sor = document.createElement('li')
+    sor.classList.add('list-group-item')
+    sor.innerText = cimzettInput + " " + datumInput + " " + osszegInput
+    //sor.setAttribute("onclick", "this.remove()")       nem jo
+    document.getElementById("osszegzes").appendChild(sor)
+
+    //teljes + atlag
+    teljesOsszeg = document.getElementById("teljesOsszeg")
+    atlagOsszeg = document.getElementById("atlagOsszeg")
+
+    let teljesOsszegValue = 0
+
+    for (let i = 0; i < szamlak.length; i++) {
+        teljesOsszegValue += parseInt(szamlak[i].osszeg)
+    }
+    let atlagOsszegValue = teljesOsszegValue / szamlak.length
+
+    teljesOsszeg.innerHTML = "Összeg: " + teljesOsszegValue
+    atlagOsszeg.innerHTML = "Átlag: " + atlagOsszegValue
+
+    //min max
+    var minOsszeg = document.getElementById("minOsszeg")
+    var maxOsszeg = document.getElementById("maxOsszeg")
+
+    if (szamlak.length == 1) {
+        minOsszegValue = szamla.osszeg
+        maxOsszegValue = szamla.osszeg
+        document.getElementById("minOsszeg").innerHTML = "Legkisebb összeg: " + minOsszegValue
+        document.getElementById("maxOsszeg").innerHTML = "Legnagyobb összeg: " + maxOsszegValue
+    }
+
+    if (minOsszegValue > parseInt(szamla.osszeg)) {
+        minOsszegValue = szamla.osszeg
+        minOsszeg.innerHTML = "Legkisebb összeg: " + minOsszegValue
+    }
+    if (maxOsszegValue < parseInt(szamla.osszeg)) {
+        maxOsszegValue = szamla.osszeg
+        maxOsszeg.innerHTML = "Legnagyobb összeg: " + maxOsszegValue
+    }
+
+    //torles gomb
+
+}
