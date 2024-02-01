ba100d4b00dcfa25ee2dab756b481e1c9c12ce28
David95406 <83376469+David95406@users.noreply.github.com>
2024-02-01 10:31:42 +0100

    Doga0201 + javitas

---
+++ 12/OL/Doga0201/src/test/java/TortaTest.java
@@ -0,0 +1,126 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TortaTest {
+    private static Torta t1;
+    private static Torta t2;
+    private static Torta t3;
+    private static Torta t4;
+    private static Torta t5;
+
+    @BeforeAll
+    static void beforeAll() {
+        t1 = new Torta(3, true, "negyzet");
+        t3 = new Torta (3, true, "kor");
+        t2 = new Torta();
+        t4 = new Torta(4, true, "vonal");
+        t5 = new Torta(-2, false, "habok");
+    }
+
+    /*
+    @Test
+    void konstruktorParameteresTest() {
+        Assertions.assertEquals(3, t3.getEmeletekSzama(), "Hiba a parametes konstuktorban!");
+    }
+
+    @Test
+    void konstruktorParameterNelkuliTest() {
+        Assertions.assertEquals("kör", t2.getAlak());
+    }
+     */
+
+    //konstuktorTest javitas
+    @Test
+    void parameteresKonstruktorTest() {
+        Assertions.assertEquals("vonal", t4.getAlak(), "Hibas a konstruktor!");
+        Assertions.assertTrue(t4.isKremes(), "Hibas a konstruktor!");
+        Assertions.assertEquals(4, t4.getEmeletekSzama(), "Hibas a konstruktor");
+    }
+
+    @Test
+    void parameterNelkuliKontruktor() {
+
+    }
+
+    @Test
+    void hibasKonstrukorTest() {
+        //fail stb...
+    }
+
+    @Test
+    void toStringTest() {
+        Assertions.assertEquals("{kor;3;true}", t3.toString(), "A toString metodus hibasan irja ki az adatokat!");
+    }
+
+    @Test
+    void ujEmeletTest() {
+        int regiEmeletekSzama = t1.getEmeletekSzama();
+        t1.ujEmelet();
+        Assertions.assertEquals(regiEmeletekSzama + 1, t1.getEmeletekSzama(), "Az ujEmelet metodus nem noveli az emeletek szamat!");
+    }
+
+    @Test
+    void setAlakTest() {
+        t1.setAlak("gula");
+        Assertions.assertEquals("gula", t1.getAlak(), "A setAlak metodus nem valtoztatja meg a torta alakjat!");
+    }
+
+    @Test
+    void setAlakHibasTest() {
+        try {
+            t1.setAlak("");
+        } catch (IllegalArgumentException iae) {
+            return;
+        }
+        Assertions.fail("A setAlak metodus elfogad nem letezo alakot is!");
+    }
+
+    @Test
+    void kremmelMegkenNemKremesTortaTest() {
+        try {
+            t2.kremmelMegken();
+        } catch (TortaException te) {
+            Assertions.fail("A kremmelMegken metodus nem keni meg a nem kremes tortat!");
+        }
+
+    }
+
+    @Test
+    void kremmelMegkenKremesTortaTest() {
+        try {
+            t1.kremmelMegken();//!
+            Assertions.assertTrue(t1.isKremes(), "A kremmelMegken metodus nem keni meg a tortat!");
+        } catch (TortaException te) {
+            return;
+        }
+        Assertions.fail("A kremmelMegken metodus megkeni a kremes tortat!");
+    }
+
+    /* valami javitas (hibas)
+    @Test
+    void kremmelMegkenTest() {
+        try {
+            t2.kremmelMegken();
+        } catch (TortaException te) {
+            Assertions.fail("Hiba a nem kremes torta megkenesenel!");
+        }
+        Assertions.assertTrue(t2.isKremes(), "Hiba a kremmelMegken metodusnal!");
+    }
+     */
+
+    @Test
+    void mennyiKaloriaNemKremesTortaTest() {
+        t2.setKremes(false);
+        Assertions.assertEquals(1000, t2.mennyiKaloria(), "A mennyiKaloria metodus hibasan szamol nem kremes torta eseten!");
+    }
+
+    @Test
+    void mennyiKaloriaKremesTortaTest(){
+        Assertions.assertEquals(6000, t3.mennyiKaloria(), "A mennyiKaloria metodus hibasan szamol krmes torta eseten!");
+    }
+
+}
---
+++ 12/OL/Doga0201Jav/src/main/java/Torta.java
@@ -0,0 +1,65 @@
+public class Torta {
+    private int emeletekSzama;
+    private boolean isKremes;
+    private String alak;
+
+    public Torta(int emeletekSzama, boolean isKremes, String alak) throws IllegalArgumentException {
+        this.setEmeletekSzama(emeletekSzama);
+        this.setKremes(isKremes);
+        this.setAlak(alak);
+    }
+
+    public Torta() throws IllegalArgumentException {
+        this(1, false, "kör");
+    }
+
+    @Override
+    public String toString() {
+        return "{" + getAlak() + ";" + getEmeletekSzama() + ";" + isKremes() + "}";
+    }
+
+    public void kremmelMegken() throws TortaException {
+        if (isKremes()) {
+            throw new TortaException("A torta mar kremes!");
+        }
+        setKremes(true);
+    }
+
+    public int mennyiKaloria() {
+        if (isKremes()) {
+            return (getEmeletekSzama() * 1000) * 2;
+        }
+        return getEmeletekSzama() * 1000;
+    }
+
+    public void ujEmelet() {
+        setEmeletekSzama(getEmeletekSzama() + 1);
+    }
+
+    public int getEmeletekSzama() {
+        return emeletekSzama;
+    }
+
+    public void setEmeletekSzama(int emeletekSzama) throws IllegalArgumentException {
+        if (emeletekSzama <= 0) {
+            throw new IllegalArgumentException("Az emeletek szama nem lehet 0 vagy negativ szam!");
+        }
+        this.emeletekSzama = emeletekSzama;
+    }
+
+    public boolean isKremes() {
+        return isKremes;
+    }
+
+    public void setKremes(boolean kremes) {
+        isKremes = kremes;
+    }
+
+    public String getAlak() {
+        return alak;
+    }
+
+    public void setAlak(String alak) {
+        this.alak = alak;
+    }
+}
