b10638441f06656bab3d564fb1ec378901176a8b
David95406 <83376469+David95406@users.noreply.github.com>
2024-02-12 11:26:07 +0100

    Bukkmaraton0212

--- 12/JavaFX/BukkMaraton/src/main/java/com/example/bukkmaraton/HelloController.java
+++ 12/JavaFX/BukkMaraton/src/main/java/com/example/bukkmaraton/HelloController.java
@@ -1,14 +1,53 @@
 package com.example.bukkmaraton;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ComboBox<String> tavChoice;
+    @FXML
+    private Label kmhAtlagsebesseg;
+    @FXML
+    private Label msAtlagsebesseg;
+    @FXML
+    private TextField idoInput;
+    @FXML
+    private Button szamolButton;
+
+    //  v=s/t
+    @FXML
+    private void initialize() {
+        tavChoice.setItems(FXCollections.observableArrayList(
+                "Mini - 16km",
+                "Rövid - 38km",
+                "Pedelec - 54km",
+                "Közép - 57km",
+                "Hosszú - 94mk"
+        ));
+        tavChoice.getSelectionModel().select("Közép - 57km");
+    }
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void calculate() {
+        int[] tavok = {16, 38, 54, 57, 94};
+
+        int tav = tavok[tavChoice.getSelectionModel().getSelectedIndex()];
+        String[] ido = idoInput.getText().split(":");
+        int ora = Integer.parseInt(ido[0]);
+        int perc = Integer.parseInt(ido[1]);
+        int mp = Integer.parseInt(ido[2]);
+
+        double idoOra = ora + ((double) perc / 60) + ((double) mp / 3600);
+        double kmh = tav / idoOra;
+        double ms = kmh / 3.6;
+        kmhAtlagsebesseg.setText(String.format("%.2f", kmh) + " km/h");
+        msAtlagsebesseg.setText(String.format("%.2f", ms) + " m/s");
     }
+
+
+
+
+
 }
