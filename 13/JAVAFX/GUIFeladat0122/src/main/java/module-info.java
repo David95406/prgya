1de75b70f96d2372d4a3ea61530327bbaf0237c2
David95406 <83376469+David95406@users.noreply.github.com>
2025-01-22 20:01:02 +0100

    GUI feladat

---
+++ 13/JAVAFX/GUIFeladat0122/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.mangec.guifeladat0122 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.mangec.guifeladat0122 to javafx.fxml;
+    exports com.mangec.guifeladat0122;
+}
